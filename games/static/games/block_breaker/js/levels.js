83561727fb3abc3c5baa78eeaeb051b3650da98c
YahanZHANG <yahanyahan2013@gmail.com>
2026-08-09 11:50:28 +0200

    Add stages 6 to 10

--- games/static/games/block_breaker/js/levels.js
+++ games/static/games/block_breaker/js/levels.js
@@ -138,4 +138,149 @@ export const LEVELS = [
             "5-7": "wide-paddle",
         },
     },
+        
+    {
+        number: 6,
+        name: "ツインタワー",
+        ballSpeed: 7.0,
+        powerupAccess: POWERUP_ACCESS.ALL,
+        comboEnabled: true,
+        movingBricks: true,
+        brickMovementSpeed: 1.2,
+        brickMovementRange: 20,
+
+        layout: [
+            [2, 2, 0, 3, 3, 0, 2, 2],
+            [2, 4, 0, 1, 1, 0, 4, 2],
+            [5, 2, 0, 2, 2, 0, 2, 5],
+            [2, 1, 0, 3, 3, 0, 1, 2],
+            [4, 2, 0, 1, 1, 0, 2, 4],
+            [2, 5, 0, 2, 2, 0, 5, 2],
+        ],
+
+        powerups: {
+            "1-1": "fireball",
+            "1-6": "wide-paddle",
+            "4-0": "laser",
+            "4-7": "fireball",
+        },
+    },
+
+    {
+        number: 7,
+        name: "クロスファイア",
+        ballSpeed: 7.3,
+        powerupAccess: POWERUP_ACCESS.ALL,
+        comboEnabled: true,
+        movingBricks: true,
+        brickMovementSpeed: 1.35,
+        brickMovementRange: 24,
+
+        layout: [
+            [2, 0, 0, 5, 5, 0, 0, 2],
+            [0, 2, 4, 1, 1, 4, 2, 0],
+            [0, 4, 3, 2, 2, 3, 4, 0],
+            [5, 1, 2, 3, 3, 2, 1, 5],
+            [0, 4, 3, 2, 2, 3, 4, 0],
+            [0, 2, 4, 1, 1, 4, 2, 0],
+            [2, 0, 0, 5, 5, 0, 0, 2],
+        ],
+
+        powerups: {
+            "1-2": "laser",
+            "1-5": "fireball",
+            "4-1": "wide-paddle",
+            "4-6": "laser",
+            "5-2": "fireball",
+            "5-5": "wide-paddle",
+        },
+    },
+
+    {
+        number: 8,
+        name: "スネークライン",
+        ballSpeed: 7.4,
+        powerupAccess: POWERUP_ACCESS.ALL,
+        comboEnabled: true,
+        movingBricks: true,
+        brickMovementSpeed: 1.45,
+        brickMovementRange: 18,
+
+        layout: [
+            [2, 2, 2, 4, 5, 2, 2, 2],
+            [0, 0, 0, 0, 1, 0, 0, 0],
+            [2, 2, 5, 4, 2, 2, 2, 0],
+            [1, 0, 0, 0, 0, 0, 0, 0],
+            [2, 2, 2, 2, 4, 5, 2, 2],
+            [0, 0, 0, 0, 0, 0, 0, 1],
+            [0, 2, 2, 5, 4, 2, 2, 2],
+        ],
+
+        powerups: {
+            "0-3": "fireball",
+            "2-3": "laser",
+            "4-4": "wide-paddle",
+            "6-4": "fireball",
+        },
+    },
+
+    {
+        number: 9,
+        name: "ブラックホール",
+        ballSpeed: 7.6,
+        powerupAccess: POWERUP_ACCESS.ALL,
+        comboEnabled: true,
+        movingBricks: true,
+        brickMovementSpeed: 1.5,
+        brickMovementRange: 22,
+
+        layout: [
+            [2, 2, 2, 5, 5, 2, 2, 2],
+            [2, 4, 1, 3, 3, 1, 4, 2],
+            [2, 1, 3, 0, 0, 3, 1, 2],
+            [5, 3, 0, 0, 0, 0, 3, 5],
+            [5, 3, 0, 0, 0, 0, 3, 5],
+            [2, 1, 3, 0, 0, 3, 1, 2],
+            [2, 4, 1, 3, 3, 1, 4, 2],
+            [2, 2, 2, 5, 5, 2, 2, 2],
+        ],
+
+        powerups: {
+            "1-1": "laser",
+            "1-6": "wide-paddle",
+            "6-1": "fireball",
+            "6-6": "laser",
+        },
+    },
+
+    {
+        number: 10,
+        name: "ラスト・カオス",
+        ballSpeed: 7.9,
+        powerupAccess: POWERUP_ACCESS.ALL,
+        comboEnabled: true,
+        movingBricks: true,
+        brickMovementSpeed: 1.65,
+        brickMovementRange: 26,
+
+        layout: [
+            [5, 2, 4, 3, 3, 4, 2, 5],
+            [2, 5, 2, 1, 1, 2, 5, 2],
+            [4, 2, 3, 5, 5, 3, 2, 4],
+            [3, 1, 5, 2, 2, 5, 1, 3],
+            [3, 1, 5, 2, 2, 5, 1, 3],
+            [4, 2, 3, 5, 5, 3, 2, 4],
+            [2, 5, 2, 1, 1, 2, 5, 2],
+            [5, 2, 4, 3, 3, 4, 2, 5],
+        ],
+
+        powerups: {
+            "0-2": "fireball",
+            "0-5": "laser",
+            "2-0": "wide-paddle",
+            "2-7": "fireball",
+            "5-0": "laser",
+            "5-7": "wide-paddle",
+        },
+    },
 ];
