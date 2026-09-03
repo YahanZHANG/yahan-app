8a24e7fe1c1ff3f2c02a880ad7f0df9e1aec30c1
YahanZHANG <yahanyahan2013@gmail.com>
2026-09-03 17:25:33 +0200

    change password

--- config/urls.py
+++ config/urls.py
@@ -10,6 +10,23 @@ urlpatterns = [
     ),
 
     path(
+        "password/change/",
+        auth_views.PasswordChangeView.as_view(
+            template_name="registration/password_change_form.html",
+            success_url="/password/change/done/",
+        ),
+        name="password_change",
+    ),
+
+    path(
+        "password/change/done/",
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name="registration/password_change_done.html",
+        ),
+        name="password_change_done",
+    ),
+
+    path(
         "accounts/login/",
         auth_views.LoginView.as_view(
             template_name="registration/login.html",
